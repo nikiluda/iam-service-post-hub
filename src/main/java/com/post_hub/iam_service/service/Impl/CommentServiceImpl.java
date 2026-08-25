cd3abf14afd5ca477f5dc0dc2baf77ca939af106
nikiluda <zhanlin_nikita27@mail.ru>
2026-08-25 13:58:44 +0600

    refactor

--- src/main/java/com/post_hub/iam_service/service/Impl/CommentServiceImpl.java
+++ src/main/java/com/post_hub/iam_service/service/Impl/CommentServiceImpl.java
@@ -2,7 +2,6 @@ package com.post_hub.iam_service.service.Impl;
 
 import com.post_hub.iam_service.service.CommentService;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
 
